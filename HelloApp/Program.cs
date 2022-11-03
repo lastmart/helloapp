7f594bfe93f2bd215dd66f1956523fdd5fd00caf
white <white@example.com>
2022-11-03 18:49:11 +0500

    greet white

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -8,5 +8,10 @@ namespace Programm
         {
             Console.WriteLine("hello");
         }
+
+        static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
     }
 }
